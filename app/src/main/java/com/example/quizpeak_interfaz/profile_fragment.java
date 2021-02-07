a6fb9a3f0a9a0dad640bea16d019d81e982ced30
PamelaCanul <pamelacanul05@gmail.com>
2021-02-06 20:30:00 -0600

    Version 1

---
+++ app/src/main/java/com/example/quizpeak_interfaz/profile_fragment.java
@@ -0,0 +1,36 @@
+package com.example.quizpeak_interfaz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class profile_fragment extends Fragment {
+
+    FloatingActionButton fab;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
+
+}
